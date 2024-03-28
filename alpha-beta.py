b0de102d927ebedcee1c1a0b0d0ba56877409916
Krisjanis Bergmanis <Krisjanis.Bergmanis@edu.rtu.lv>
2024-03-28 19:45:53 +0200

    Comment out test lines

--- alpha-beta.py
+++ alpha-beta.py
@@ -33,10 +33,10 @@ def alpha_beta(node, alpha, beta, max_visibility):
 alpha = float('-inf')
 beta = float('inf')
 
-init_tree('1010')
-gen_node(tree[0], MAX_VISIBILITY) # This is used for testing
+# init_tree('1010') # This is used for testing
+# gen_node(tree[0], MAX_VISIBILITY) # This is used for testing
 # Perform alpha-beta pruning and print each step
-print("Alpha-Beta steps:")
-print(f"Initial Alpha: {alpha}, Initial Beta: {beta}")
-result = alpha_beta(tree[0], alpha, beta, MAX_VISIBILITY)
-print(f"Optimal Value: {result}")
+# print("Alpha-Beta steps:") # This is used for testing
+# print(f"Initial Alpha: {alpha}, Initial Beta: {beta}") # This is used for testing
+# result = alpha_beta(tree[0], alpha, beta, MAX_VISIBILITY) # This is used for testing
+# print(f"Optimal Value: {result}") # This is used for testing
